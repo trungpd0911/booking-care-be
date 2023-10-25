14b539de7bfa6138442aa7ba044c4164015d9cc2
trungphan123456 <trungphan9dxd@gmail.com>
2023-10-25 22:11:21 +0700

    update CRU user

--- src/routes/web.js
+++ src/routes/web.js
@@ -7,6 +7,12 @@ let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
     router.get("/crud", homeController.getCRUD);
     router.post('/post-crud', homeController.postCRUD);
+    // create new user
+    router.get('/get-crud', homeController.displayGetCRUD);
+    // get all user
+    router.get('/edit-crud', homeController.getEditCRUD);
+    router.post('/put-crud', homeController.putCRUD);
+    // edit user
 
     return app.use("/", router);
 };
